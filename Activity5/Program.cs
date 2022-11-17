d9209c016b400b98d6852bdbb219dc9c0a4795b6
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2022-11-17 16:27:32 +0700

    create class node

--- Activity5/Program.cs
+++ Activity5/Program.cs
@@ -5,6 +5,12 @@ namespace DoublyLinkedList
     class Node
     {
         /*Node class represents the node of doubly linked list.
-         * It consist of the information part and
+         * It consist of the information part and links to
+         * its succeeding and preceeding nodes
+         * in terms of next and previous nodes.*/
+        public int rollNumber;
+        public string name;
+        public Node next;/*points to the suceeding node*/
+        public Node prev;/*points to the suceeding node*/
     }
 }
