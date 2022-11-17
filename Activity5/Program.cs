5a2cd41d450ff28e48c44811a0afb969239734b5
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2022-11-17 16:20:53 +0700

    Add project files.

---
+++ Activity5/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    class Node
+    {
+        /*Node class represents the node of doubly linked list.
+         * It consist of the information part and
+    }
+}
